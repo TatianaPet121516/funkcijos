b8d74b5c21728d9f91438083398855e1773627f3
TatianaPet121516 <167982496+TatianaPet121516@users.noreply.github.com>
2024-05-06 11:55:23 +0300

    Update homework1.js

--- homework1.js
+++ homework1.js
@@ -194,3 +194,37 @@ function price(value) {
 console.log(price(aa));     //121
 console.log(price(bb));     //181.5
 console.log(price(cc));     //657.03
+
+
+console.log(" -- BMI -- ");
+let massMark = 78;
+let massJohn = 92;
+let heightMark = 1.69;
+let heightJohn = 1.95;
+
+BMIMark = massMark / heightMark ** 2;
+BMIJohn = massJohn / heightJohn ** 2;
+
+//console.log("BMI Mark = ", BMIMark);
+//console.log("BMI John = ", BMIJohn);
+
+if (BMIMark > BMIJohn) { 
+    console.log("Mark's BMI (", BMIMark, ") is higher than John's (", BMIJohn, ").");
+} else { 
+    console.log("John's BMI (", BMIJohn, ") is higher than Mark's (", BMIMark, ")."); 
+}
+
+console.log(" ----- ");
+massMark = 95;
+massJohn = 85;
+heightMark = 1.88;
+heightJohn = 1.76;
+
+BMIMark = massMark / heightMark ** 2;
+BMIJohn = massJohn / heightJohn ** 2;
+
+if (BMIMark > BMIJohn) { 
+    console.log("Mark's BMI (", BMIMark, ") is higher than John's (", BMIJohn, ").");
+} else { 
+    console.log("John's BMI (", BMIJohn, ") is higher than Mark's (", BMIMark, ")."); 
+}
